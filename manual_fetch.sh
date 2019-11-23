91072e8fa41301477b8d2d03222be3907bbd6993
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2019-11-23 10:18:45 +0900

    Apply git submodule

--- manual_fetch.sh
+++ manual_fetch.sh
@@ -2,6 +2,7 @@
 
 echo -n "Enter Your ACCESS_TOKEN: "
 read -s access_token
+echo ""
 read -e -p "Enter Your DB_PATH: " db_path
 read -e -p "Enter Your REPORT_PATH: " report_path
 read -e -p "Download database from Dropbox? (y/N): " download
@@ -40,12 +41,9 @@ fi
 # exist prarxivate
 if [ ! -d ./prarxivate ]
 then 
-    echo "Clone prarxivate"
-    git clone https://github.com/GzuPark/prarxivate.git
-    cp automation.py prarxivate/automation.py
+    echo "Add submodule prarxivate"
+    git submodule add https://github.com/GzuPark/prarxivate.git
 fi
-
-cd prarxivate
 
 # download db
 if [[ $download == "y" || $download == "Y" ]]
@@ -59,7 +57,7 @@ then
 fi
 
 # fetch
-python fetch_papers.py -si $si -mi $mi -sq cat:cs.CV+OR+cat:cs.RO+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML
+python prarxivate/fetch_papers.py -si $si -mi $mi -break 0 -sq cat:cs.CV+OR+cat:cs.RO+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML
 
 # upload db
 if [[ $upload == "y" || $upload == "Y" ]]
